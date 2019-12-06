1f699c0399725b27be8d69ebaf917c3552d37101
Vessalius <464068562@qq.com>
2019-12-06 10:51:36 +0800

    归并排序

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,6 +1,6 @@
 # 目录
 
-* [前言](README.md)
+* [前言](chapter/README.md)
 * [算法基础](chapter/01/chapter1.md)
     * [插入排序](chapter/01/insertion-sort.md)
     * [选择排序](chapter/01/selection-sort.md)
--- _book/code/01/code.php
+++ _book/code/01/code.php
@@ -104,3 +104,60 @@ function selection_sort($arr,$result = []){
 
 $arr = [1,2,6,7,1,2,3,2,1];
 print_r(selection_sort($arr));
+
+/**
+ * 分治法
+ * @param array $arr 原始数组
+ * @param int $p，$q，$r 满足$p <= $q < $r
+ */
+function merge(&$arr, $p, $q, $r){
+    //定义两个子数组（都已知为升序排列）
+    $n1 = $q - $p + 1;
+    $n2 = $r - $q;
+    $L = [];
+    $R = [];
+    for($i = 0;$i < $n1;++$i){
+        $L[$i] = $arr[$p + $i];
+    }
+    for($j = 0;$j < $n2;++$j){
+        $R[$j] = $arr[$q + $j + 1];
+    }
+    //比较两个子数组中第一个元素的较小值放入原数组对应位置中
+    $i = 0;
+    $j = 0;
+    for($k = $p;$k <= $r;++$k){
+        //考虑有一个子数组取完时的情况
+        if($j == $n2 ||($L[$i] <= $R[$j] && $i < $n1)){
+            $arr[$k] = $L[$i];
+            ++$i;
+        }else{
+            $arr[$k] = $R[$j];
+            ++$j;
+        }
+    }
+    return $arr;
+}
+
+//$arr = [1,3,4,3,1,3,8,9,2,4,5,7,1,2,3,6,4,3,7,8,4,2];
+//merge($arr, 0, 10, 21);
+//print_r($arr);
+
+
+/**
+ * 利用分治法进行归并排序
+ * @param array $arr
+ * @param int $p,$r
+ */
+function merge_sort(&$arr, $p, $r){
+    if($p < $r){
+        //取中间数
+        $q = intval(($p + $r) / 2);
+        merge_sort($arr, $p, $q);
+        merge_sort($arr, $q + 1, $r);
+        merge($arr, $p, $q, $r);
+    }
+}
+
+$arr = [1,3,4,3,1,3,8,9,2,4,5,7,1,2,3,6,4,3,7,8,4,2];
+merge_sort($arr, 0, 21);
+print_r($arr);
